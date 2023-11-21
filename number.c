6ea84bb2af097d35ce9b3d3a3184223d87e8919e
Mohamedeshahat <mohamedelshahat373@gmail.com>
2023-11-21 08:36:24 -0800

    1

---
+++ number.c
@@ -0,0 +1,74 @@
+#include"main.h"
+/**
+ * convert - functuio
+ * @num: number
+ * @base: base
+ * @flags: flag
+ * @params: parameters
+ * Return: void
+ */
+char *convert(long int num, int base, int flags, params_t *params)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+	(void)params;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+	}
+	array = flags & CONVERT_LOWERCASE ? "123456789abcdef" : "123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr == array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+/**
+ * print_unsigned - print string
+ * @ap: arguments
+ * @params: parameters
+ * Return: voud
+ */
+int print_unsigned(va_list ap, params_t *params)
+{
+	unsigned long l;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+	params->unsign = 1;
+	return (print_number(convert(1, 10, CONVERT_UNSIGNED, params), params));
+}
+
+/**
+ * print_address - print the address
+ * @ap: argument
+ * @params: parameter
+ * Return: bytes number
+ */
+int print_address(va_list ap, params_t *params)
+{
+	unsigned long int n = va_arg(ap, unsigned long int);
+	char *str;
+
+	if (!n)
+		return (_puts("(nil)"));
+	str = convert(n, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	*--str = 'x';
+	*--str = '0';
+	return (print_number(str, params));
+}
+
